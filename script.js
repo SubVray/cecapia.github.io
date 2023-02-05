be901ec5ddd027a8fe4e7db66f30bc7221576284
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-05 12:08:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -213,7 +213,7 @@ const sendInfo = async () => {
       frontImg: front,
       backImg: back,
     };
-      fetch("http://localhost:5000/data", {
+      fetch("https://localhost:5000/data", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
