a9c771ee218c35200f9d1d7e91d486d9d7c5a13d
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-04 20:29:14 -0600

    cecapia

--- app.js
+++ app.js
@@ -19,10 +19,69 @@ saveButton.addEventListener("click", function () {
     cedula: textCedula.value,
     firma: dataURL,
   };
-  console.log(dataURL);
+  console.log(user);
   // Save dataURL to database or send to server
 });
 
+const form = document.querySelector("#upload-form");
+const cameraBox = document.querySelector("#camera-box");
+const cameraStream = document.querySelector("#camera-stream");
+const captureButton = document.querySelector("#capture-button");
+const frontInput = document.querySelector("#front-input");
+const backInput = document.querySelector("#back-input");
+const switchButton = document.querySelector("#switch-button");
 
+let isBack = false;
+if (!isBack) {
+  console.log("parte frontal");
+} else {
+  console.log("parte trasera");
+}
 
+async function startCamera() {
+  form.classList.toggle("d-none");
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "user" },
+  });
 
+  cameraStream.srcObject = stream;
+  cameraStream.play();
+}
+
+captureButton.addEventListener("click", function () {
+  const canvas = document.createElement("canvas");
+  canvas.width = cameraStream.videoWidth;
+  canvas.height = cameraStream.videoHeight;
+
+  const context = canvas.getContext("2d");
+  context.drawImage(cameraStream, 0, 0);
+
+  const photo = canvas.toDataURL("image/jpeg");
+
+  if (isBack) {
+    backInput.value = photo;
+  } else {
+    frontInput.value = photo;
+  }
+});
+
+switchButton.addEventListener("click", function () {
+  isBack = !isBack;
+  if (!isBack) {
+    document.getElementById("switch-button").innerHTML =
+      "Cambiar a la parte trasera";
+  } else {
+    document.getElementById("switch-button").innerHTML =
+      "Cambiar a la parte frontal";
+  }
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const front = frontInput.value;
+  const back = backInput.value;
+  console.log(front, back);
+
+  // Do something with front and back (e.g. send to server)
+});
