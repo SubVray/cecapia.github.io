5bfbb544a8a86cac82875ee9c015ded521b42385
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-04 21:42:06 -0600

    x

--- script.js
+++ script.js
@@ -1,98 +1,106 @@
-feather.replace();
+const canvas = document.querySelector("canvas");
+const clearButton = document.querySelector("#clear");
+const saveButton = document.querySelector("#save");
+const textCedula = document.querySelector("#text-cedula");
 
-const controls = document.querySelector('.controls');
-const cameraOptions = document.querySelector('.video-options>select');
-const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
-const screenshotImage = document.querySelector('img');
-const buttons = [...controls.querySelectorAll('button')];
-let streamStarted = false;
+const signaturePad = new SignaturePad(canvas, {
+  minWidth: 1,
+  maxWidth: 1,
+  penColor: "blue",
+});
 
-const [play, pause, screenshot] = buttons;
+clearButton.addEventListener("click", function () {
+  signaturePad.clear();
+});
 
-const constraints = {
-  video: {
-    width: {
-      min: 1280,
-      ideal: 1920,
-      max: 2560,
-    },
-    height: {
-      min: 720,
-      ideal: 1080,
-      max: 1440
-    },
-  }
-};
-
-cameraOptions.onchange = () => {
-  const updatedConstraints = {
-    ...constraints,
-    deviceId: {
-      exact: cameraOptions.value
-    }
+saveButton.addEventListener("click", function () {
+  const dataURL = signaturePad.toDataURL();
+  const user = {
+    cedula: textCedula.value,
+    firma: dataURL,
   };
+  console.log(user);
+  // Save dataURL to database or send to server
+});
 
-  startStream(updatedConstraints);
-};
+const form = document.querySelector("#upload-form");
+const cameraBox = document.querySelector("#camera-box");
+const cameraStream = document.querySelector("#camera-stream");
+const captureButton = document.querySelector("#capture-button");
+const frontInput = document.querySelector("#front-input");
+const backInput = document.querySelector("#back-input");
+const switchButton = document.querySelector("#switch-button");
 
-play.onclick = () => {
-  if (streamStarted) {
-    video.play();
-    play.classList.add('d-none');
-    pause.classList.remove('d-none');
-    return;
+let isBack = false;
+if (!isBack) {
+  console.log("parte frontal");
+} else {
+  console.log("parte trasera");
+}
+
+async function detectDeviceType() {
+  var isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  if (isMobile) {
+    console.log("Estás en un dispositivo móvil");
+    form.classList.toggle("d-none");
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "user" },
+    });
+
+    cameraStream.srcObject = stream;
+    cameraStream.play();
+  } else {
+    console.log("Estás en un PC");
+
+    form.classList.toggle("d-none");
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "environment" },
+    });
+
+    cameraStream.srcObject = stream;
+    cameraStream.play();
   }
-  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-    const updatedConstraints = {
-      ...constraints,
-      deviceId: {
-        exact: cameraOptions.value
-      }
-    };
-    startStream(updatedConstraints);
+}
+
+captureButton.addEventListener("click", function () {
+  const canvas = document.createElement("canvas");
+  canvas.width = cameraStream.videoWidth;
+  canvas.height = cameraStream.videoHeight;
+
+  const context = canvas.getContext("2d");
+  context.drawImage(cameraStream, 0, 0);
+
+  const photo = canvas.toDataURL("image/jpeg");
+
+  if (isBack) {
+    backInput.value = photo;
+  } else {
+    frontInput.value = photo;
   }
-};
+});
 
-const pauseStream = () => {
-  video.pause();
-  play.classList.remove('d-none');
-  pause.classList.add('d-none');
-};
+switchButton.addEventListener("click", function () {
+  isBack = !isBack;
+  if (!isBack) {
+    document.getElementById("switch-button").innerHTML =
+      "Cambiar a la parte trasera";
+  } else {
+    document.getElementById("switch-button").innerHTML =
+      "Cambiar a la parte frontal";
+  }
+});
 
-const doScreenshot = () => {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0);
-  screenshotImage.src = canvas.toDataURL('image/webp');
-  screenshotImage.classList.remove('d-none');
-};
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-pause.onclick = pauseStream;
-screenshot.onclick = doScreenshot;
+  const front = frontInput.value;
+  const back = backInput.value;
+  console.log(front, back);
 
-const startStream = async (constraints) => {
-  const stream = await navigator.mediaDevices.getUserMedia(constraints);
-  handleStream(stream);
-};
-
-
-const handleStream = (stream) => {
-  video.srcObject = stream;
-  play.classList.add('d-none');
-  pause.classList.remove('d-none');
-  screenshot.classList.remove('d-none');
-
-};
-
-
-const getCameraSelection = async () => {
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const videoDevices = devices.filter(device => device.kind === 'videoinput');
-  const options = videoDevices.map(videoDevice => {
-    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
-  });
-  cameraOptions.innerHTML = options.join('');
-};
-
-getCameraSelection();
+  // Do something with front and back (e.g. send to server)
+});
