47cfcf48a7ebfbda08a7ff04f3d5039f5bd79fda
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-04 18:34:04 -0600

    firma cecapia

---
+++ app.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector("canvas");
+const clearButton = document.querySelector("#clear");
+const saveButton = document.querySelector("#save");
+const textCedula = document.querySelector("#text-cedula");
+
+const signaturePad = new SignaturePad(canvas, {
+  minWidth: 1,
+  maxWidth: 1,
+  penColor: "blue",
+});
+
+clearButton.addEventListener("click", function () {
+  signaturePad.clear();
+});
+
+saveButton.addEventListener("click", function () {
+  const dataURL = signaturePad.toDataURL();
+  const user = {
+    cedula: textCedula.value,
+    firma: dataURL,
+  };
+  console.log(user);
+  // Save dataURL to database or send to server
+});
